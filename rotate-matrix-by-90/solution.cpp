2277d992bd637743ba60a7c34dc50948e8c6b65d
Abhijeet Gurle <abhijeetgurle@gmail.com>
2022-12-15 08:18:12 +0530

    feat: add main function

--- rotate-matrix-by-90/solution.cpp
+++ rotate-matrix-by-90/solution.cpp
@@ -1,3 +1,7 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
 void rotate(vector<vector<int>>& matrix) {
     int n = matrix.size();
 
@@ -11,3 +15,45 @@ void rotate(vector<vector<int>>& matrix) {
         reverse(matrix[i].begin(), matrix[i].end());
     }
 }
+
+int main() {
+    // [[1,2,3],[4,5,6],[7,8,9]]
+    vector<vector<int>> matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    rotate(matrix);
+    
+    for(int i=0; i<matrix.size(); i++) {
+        for(int j=0; j<matrix[i].size(); j++) {
+            cout<<matrix[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+// Time Complexity:- O(N^2)
+// Space Complexity:- O(1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
