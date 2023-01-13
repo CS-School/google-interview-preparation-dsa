bec165284e80ecbd3caa70377f7c500baf09eba5
Abhijeet Gurle <abhijeetgurle@gmail.com>
2023-01-13 15:53:38 +0530

    feat: add maximum sum subarray problem

--- maximum-subarray/solution.cpp
+++ maximum-subarray/solution.cpp
@@ -6,18 +6,18 @@ using namespace std;
 
 int maxSubArray(vector<int> &nums)
 {
-    int max_so_far = INT_MIN, max_ending_here = 0;
+    int ans = INT_MIN, max_ending_here = 0;
 
     for (int i = 0; i < nums.size(); i++)
     {
         max_ending_here = max_ending_here + nums[i];
-        if (max_so_far < max_ending_here)
-            max_so_far = max_ending_here;
+        if (ans < max_ending_here)
+            ans = max_ending_here;
 
         if (max_ending_here < 0)
             max_ending_here = 0;
     }
-    return max_so_far;
+    return ans;
 }
 
 int main()
