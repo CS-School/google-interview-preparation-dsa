50c98a58c449f60fd375cfb9f6c3d52c7c2d6157
Abhijeet Gurle <abhijeetgurle@gmail.com>
2023-04-16 09:23:12 +0530

    feat: changes in binary search

--- binary-search/solution.cpp
+++ binary-search/solution.cpp
@@ -5,16 +5,16 @@ using namespace std;
 int search(vector<int> &nums, int target)
 {
 
-    int l = 0, r = nums.size() - 1;
-    while (l <= r)
+    int start = 0, end = nums.size() - 1;
+    while (start <= end)
     {
-        int mid = l + (r - l) / 2;
+        int mid = start + (end - start) / 2;
         if (nums[mid] == target)
             return mid;
         else if (nums[mid] < target)
-            l = mid + 1;
+            start = mid + 1;
         else
-            r = mid - 1;
+            end = mid - 1;
     }
     return -1;
 }
